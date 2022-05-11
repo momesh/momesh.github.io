3011e7ffc9b04321cb57d2481cb4eb9731c66123
Gabe Conradi <gabe.conradi@gmail.com>
2022-05-11 13:43:16 -0400

    fix grouping and updating totals in map view

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,14 +1,24 @@
+var groupFeaturesBy = function (xs, keyFn) {
+  return xs.reduce(function (rv, x) {
+    (rv[keyFn(x)] = rv[keyFn(x)] || []).push(x);
+    return rv;
+  }, {});
+};
+
 var target = 'momesh-map';
+var nodeCompositeKey = function (feature) {
+  return feature.get('status') + "-" + feature.get('type');
+};
 
 var nodeHubStyle = new ol.style.Circle({
   radius: 5,
-  fill: new ol.style.Fill({color: 'blue'}),
+  fill: new ol.style.Fill({color: 'magenta'}),
   stroke: null,
 });
 
 var nodeStyle = new ol.style.Circle({
   radius: 5,
-  fill: new ol.style.Fill({color: 'red'}),
+  fill: new ol.style.Fill({color: 'blue'}),
   stroke: null,
 });
 
@@ -79,19 +89,63 @@ var styles = {
 };
 
 var styleFunction = function (feature) {
-  var color = feature.get('marker-color');
+  // TODO: handle other features other than points
+  // TODO: handle override marker-color per feature
+  // var color = feature.get('marker-color');
   var nn = feature.get('name');
   var t = feature.get('type');
+  var s = feature.get('status');
+  if (s == 'potential') {
+    return new ol.style.Style({image: nodePotentialStyle});
+  }
   switch (t) {
     case 'node':
       return new ol.style.Style({image: nodeStyle});
     case 'hub':
       return new ol.style.Style({image: nodeHubStyle});
-    case 'potential':
-      return new ol.style.Style({image: nodePotentialStyle});
   }
   return styles[feature.getGeometry().getType()];
 };
+
+function onMoveEnd(evt) {
+  var map = evt.map;
+  console.log("move end:", map);
+}
+
+function onFeaturesLoadEnd(evt) {
+  // update legend based on evt.features properties
+  // group features by properties.get('type') and 'status'
+  // count active hubs, nodes, planned
+  var buckets = groupFeaturesBy(evt.features, nodeCompositeKey);
+  console.log("features end", buckets);
+  var elems = {
+    'potential-nodes-total': 'potential-node',
+    'active-nodes-total': 'active-node',
+    'active-hubs-total': 'active-hub',
+  };
+  var setElementIdHTML = function (id, val) {
+    console.log(id);
+    document.getElementById(id).innerHTML = val;
+  };
+  var getValue = function (coll, key) {
+    if (key in coll) {
+      return coll[key].length;
+    } else {
+      return 0;
+    }
+  };
+
+  setElementIdHTML('potential-nodes-total', getValue(buckets, 'potential-node'));
+  setElementIdHTML('active-nodes-total', getValue(buckets, 'active-node'));
+  setElementIdHTML('active-hubs-total', getValue(buckets, 'active-hub'));
+}
+
+var meshSource = new ol.source.Vector({
+  url: '/assets/data/map.geojson',
+  format: new ol.format.GeoJSON(),
+});
+
+meshSource.on('featuresloadend', onFeaturesLoadEnd);
 
 var map = new ol.Map({
   target: target,
@@ -103,10 +157,7 @@ var map = new ol.Map({
       })
     }),
     new ol.layer.Vector({
-      source: new ol.source.Vector({
-        url: '/assets/data/map.geojson',
-        format: new ol.format.GeoJSON(),
-      }),
+      source: meshSource,
       style: styleFunction,
     }),
   ],
@@ -116,3 +167,8 @@ var map = new ol.Map({
   })
 });
 
+map.on('moveend', onMoveEnd);
+
+console.log("created map", map);
+//map.layers[1].addListener('featuresloadend', function (x) {
+//});
