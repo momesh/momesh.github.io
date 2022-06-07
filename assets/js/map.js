b0f9875e49bb81cbf3083b8f6fce248746526591
Gabe Conradi <gabe.conradi@gmail.com>
2022-06-07 15:04:24 -0400

    map: simple omnidirectional coverage layer

--- assets/js/map.js
+++ assets/js/map.js
@@ -88,7 +88,55 @@ var styles = {
   }),
 };
 
-var styleFunction = function (feature) {
+// create a feature showing approximate coverage of installed APs at site
+// returns a ol.Feature object
+var createMapFeatureCoverage = function (geom, radius) {
+  const feature = new ol.Feature({
+    geometry: new ol.geom.Circle(geom, radius),
+    //geometry: new ol.geom.Circle(ol.proj.fromLonLat([-91.4198, 38.8316]), radius),
+  });
+  return feature;
+};
+
+// render the style for a feature that has a coverage bubble or arc
+var coverageStyleFunction = function (feature) {
+  // TODO: figure out how to set zindex so coverage doesnt obscure nodes
+  return new ol.style.Style({
+    renderer(coordinates, state) {
+      const [[x, y], [x1, y1]] = coordinates;
+      const ctx = state.context;
+      const dx = x1 - x;
+      const dy = y1 - y;
+      const radius = Math.sqrt(dx * dx + dy * dy);
+
+      const innerRadius = 0;
+      const outerRadius = radius * 1.4;
+
+      const gradient = ctx.createRadialGradient(
+        x,
+        y,
+        innerRadius,
+        x,
+        y,
+        outerRadius
+      );
+      gradient.addColorStop(0, 'rgba(0,0,255,0.5)');
+      gradient.addColorStop(0.6, 'rgba(0,0,255,0.2)');
+      gradient.addColorStop(1, 'rgba(0,0,255,0)');
+      ctx.beginPath();
+      ctx.arc(x, y, radius, 0, 2 * Math.PI, true);
+      ctx.fillStyle = gradient;
+      ctx.fill();
+
+      ctx.arc(x, y, radius, 0, 2 * Math.PI, true);
+      ctx.strokeStyle = 'rgba(0,0,255,0.4)';
+      ctx.stroke();
+    },
+  });
+};
+
+
+var siteStyleFunction = function (feature) {
   // TODO: handle other features other than points
   // TODO: handle override marker-color per feature
   // var color = feature.get('marker-color');
@@ -119,23 +167,33 @@ var styleFunction = function (feature) {
 };
 
 function onMoveEnd(evt) {
-  var map = evt.map;
-  console.log("move end:", map);
+  //var map = evt.map;
+  //console.log("move end:", map);
 }
 
 function onFeaturesLoadEnd(evt) {
+  // compute coverage features from loaded features and push into computedCoverage
+  meshSource.forEachFeature(function (feature) {
+    let r = feature.get('radius');
+    if (r) {
+      // only features with a radius property will be rendered as a circle
+      // TODO: handle sectors with angular coverage here
+      let geo = feature.getGeometry();
+      const newFeature = createMapFeatureCoverage(geo.flatCoordinates, r);
+      computedCoverage.addFeature(newFeature);
+    }
+  });
+
   // update legend based on evt.features properties
   // group features by properties.get('type') and 'status'
   // count active hubs, nodes, planned
   var buckets = groupFeaturesBy(evt.features, nodeCompositeKey);
-  console.log("features end", buckets);
   var elems = {
     'potential-nodes-total': 'potential-node',
     'active-nodes-total': 'active-node',
     'active-hubs-total': 'active-hub',
   };
   var setElementIdHTML = function (id, val) {
-    console.log(id);
     document.getElementById(id).innerHTML = val;
   };
   var getValue = function (coll, key) {
@@ -156,6 +214,10 @@ var meshSource = new ol.source.Vector({
   format: new ol.format.GeoJSON(),
 });
 
+var computedCoverage = new ol.source.Vector({
+  format: new ol.format.GeoJSON(),
+});
+
 meshSource.on('featuresloadend', onFeaturesLoadEnd);
 
 var map = new ol.Map({
@@ -169,8 +231,15 @@ var map = new ol.Map({
     }),
     new ol.layer.Vector({
       source: meshSource,
-      style: styleFunction,
+      style: siteStyleFunction,
     }),
+    // TODO: uncomment to render coverage bubbles
+    /*
+    new ol.layer.Vector({
+      source: computedCoverage,
+      style: coverageStyleFunction,
+    }),
+    */
   ],
   view: new ol.View({
     center: ol.proj.fromLonLat([-91.4198, 38.8316]),
